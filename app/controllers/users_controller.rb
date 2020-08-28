3a653d276016825751d561312a043f8cbf9f8b60
yuri0818 <yyyy.0818.0830@gmail.com>
2020-08-28 16:54:06 +0000

    Finish login-function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,14 +9,15 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
-    else
-      render :new
-    end
+  @user = User.new(user_params)
+  if @user.save
+    log_in @user # 保存成功後、ログインします。
+    flash[:success] = '新規作成に成功しました。'
+    redirect_to @user
+  else
+    render :new
   end
+end
 
   private
 
@@ -24,3 +25,9 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
+
+
+
+
+
+
